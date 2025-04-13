7b3ee3eaef32605c55db1ecaf92b86e05a1eccc7
italoalcantaraa <italoalcantaraa6@gmail.com>
2025-04-13 18:12:28 -0300

    opa

--- Back_End/demo/src/main/java/com/example/demo/model/Customer.java
+++ Back_End/demo/src/main/java/com/example/demo/model/Customer.java
@@ -1,5 +1,27 @@
 package com.example.demo.model;
 
-public class Customer {
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Table(name = "tb_customer")
+@Entity
+@Data // Getters, Setter, ToString, Constructor, etc...
+public class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 8)
+    private String cpf;
 }
