3474a40505fdd5e9ed1e104c54287b00b440d61c
italoalcantaraa <italoalcantaraa6@gmail.com>
2025-05-01 22:28:52 -0300

    f

--- Back_End/demo/src/main/java/com/example/demo/validations/PersonValidation.java
+++ Back_End/demo/src/main/java/com/example/demo/validations/PersonValidation.java
@@ -13,17 +13,34 @@ public class PersonValidation {
     List<String> invalidFiels = new ArrayList<>();
 
     public void create(Person person) {
-        if(isNullOrEmpty(person.getEmail())) invalidFiels.add("E-mail");
-        if(isNullOrEmpty(person.getName())) invalidFiels.add("Nome");
-        if(isNullOrEmpty(person.getPassword())) invalidFiels.add("Senha");
-        if(isNullOrEmpty(person.getTelephone())) invalidFiels.add("Telefone");
+        // if(isNullOrEmpty(person.getEmail())) invalidFiels.add("E-mail");
+        // if(isNullOrEmpty(person.getName())) invalidFiels.add("Nome");
+        // if(isNullOrEmpty(person.getPassword())) invalidFiels.add("Senha");
+        // if(isNullOrEmpty(person.getTelephone())) invalidFiels.add("Telefone");
 
-        // inserir as validações de senha
+        
 
-        if(!invalidFiels.isEmpty()) 
-            throw new IllegalArgumentException("Campos inválidos: " + invalidFiels);  
-
+        if(!person.getPassword().matches(".*[A-Z].*")){
+            invalidFiels.add("Deve conter letras maiusculas");
         }
+
+        System.out.println(person.getPassword());
+
+        // if (!person.getPassword().matches("(?=.*[@])")) {//falta carater especial (?=.*[!@#$%^&*()_+\\-=\$$\$${};':\"|,.<>?]).{8,}$") 
+        //     invalidFiels.add("Deve conter @");
+        // }
+        // if(!person.getPassword().matches("(?=.*\\\\d).{8,}$")){
+        //     invalidFiels.add("Deve conter número");
+        // }
+
+
+        if(!invalidFiels.isEmpty()) {
+            invalidFiels.remove(invalidFiels.size() - 1);
+            throw new IllegalArgumentException("Campos inválidos: " + invalidFiels);  
+        } 
+
+        
+    }
 
     public boolean isNullOrEmpty(String data) {
         return data.isEmpty() || data == null;
