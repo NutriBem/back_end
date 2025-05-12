e5e095728428bf1098cd7b9cf82887775c12758b
italoalcantaraa <italoalcantaraa6@gmail.com>
2025-05-12 13:13:07 -0300

    agenda relationship with nutritionist

---
+++ Back_End/demo/src/main/java/com/example/demo/model/Agenda.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class Agenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_agenda")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_nutritionist")
+    private Nutritionist nutritionist;
+
+    @Column(name = "local_date")
+    private LocalDate localDate;
+
+    @Column(name = "local_time")
+    private LocalTime localTime;
+
+    @Column(insertable = true)
+    private Boolean disponibility;
+}
--- Back_End/demo/src/main/java/com/example/demo/model/Nutritionist.java
+++ Back_End/demo/src/main/java/com/example/demo/model/Nutritionist.java
@@ -1,7 +1,11 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,4 +18,7 @@ public class Nutritionist extends Person {
     
     @Column(nullable = false, unique = true)
     private String crm;
+
+    @OneToMany(mappedBy = "nutritionist")
+    List<Agenda> agendaList;
 }
