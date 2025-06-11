d653f8e2adac6e4c4374df9aedff2f2e7378ae09
RodrigoDig <rodrigogomesgoncalves5367@gmail.com>
2025-06-11 12:50:45 -0300

    Ajuste para deletar nutricionista

--- Back_End/demo/src/main/java/com/example/demo/repository/AgendaRepository.java
+++ Back_End/demo/src/main/java/com/example/demo/repository/AgendaRepository.java
@@ -1,13 +1,18 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.Agenda;
 import com.example.demo.model.Nutritionist;
 
-public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+public interface AgendaRepository extends JpaRepository<Agenda, UUID> {
+
     List<Agenda> findByNutritionist(Nutritionist nutritionist);
-    boolean existsById(Long id);
+
+    @Transactional
+    void deleteByNutritionist(Nutritionist nutritionist);
 }
